2740ff2bd33033f0491cd0b1cfea279d5df94325
khuong <khuong@tgdd.vn>
2024-04-25 00:20:42 +0700

    commit

--- src/main/java/com/nhk/MainClass.java
+++ src/main/java/com/nhk/MainClass.java
@@ -1,19 +1,31 @@
 package com.nhk;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import io.javalin.Javalin;
 import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.template.JavalinFreemarker;
 
 public class MainClass {
 
 	public static void main(String[] args) {
+//		Javalin app = Javalin.create(config -> {
+//            config.staticFiles.add(staticFiles -> {
+//            	staticFiles.directory = "/public";
+//            	staticFiles.location = Location.CLASSPATH;
+//            });
+//        }).start(8080);
+		
 		Javalin app = Javalin.create(config -> {
-            config.staticFiles.add(staticFiles -> {
-            	staticFiles.directory = "/public";
-            	staticFiles.location = Location.CLASSPATH;
-            });
+			config.fileRenderer(new JavalinFreemarker());
         }).start(8080);
 		
-		
+		app.get("/",  ctx -> { // will match anything starting with /path/
+			Map<String, Object> root = new HashMap<>();
+			root.put("message", "hello");
+			ctx.render("/templates/freemarker/test.ftl", root);
+		});
 
 	}
 
--- src/main/java/com/nhk/OmeglinMain.java
+++ src/main/java/com/nhk/OmeglinMain.java
@@ -6,7 +6,7 @@ import io.javalin.http.staticfiles.Location;
 public class OmeglinMain {
     public static void main(String[] args) {
         Javalin.create(config -> {
-            config.staticFiles.add("src/main/resources/public", Location.EXTERNAL);
+            config.staticFiles.add("/public", Location.CLASSPATH);
             config.router.mount(router -> {
                 router.ws("/api/matchmaking", Matchmaking::websocket);
             });
